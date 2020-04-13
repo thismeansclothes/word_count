9ae1fbe70c492fe777a1de98bde79b345c042d51
Uisang Lee <dmltkdzlem@naver.com>
2020-04-13 17:26:22 +0900

    final

--- firstproject/urls.py
+++ firstproject/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 import wordcount.views
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', wordcount.views.home, name="home"),
+    path('about/', wordcount.views.about, name="about"),
+    path('count/', wordcount.views.count, name="count"),
 ]
 
--- wordcount/views.py
+++ wordcount/views.py
@@ -4,3 +4,19 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'wordcount/home.html')
+
+def about(request):
+    return render(request, 'wordcount/about.html')
+
+def count(request):
+    full_text = request.GET['fulltext']
+    word_list = full_text.split()
+    word_dictionary = {}
+
+    for word in word_list:
+        if word in word_list:
+            if word in word_dictionary:
+                word_dictionary[word] += 1
+            else:
+                word_dictionary[word] = 1
+    return render(request, 'wordcount/count.html', {'fulltext': full_text, 'total': len(word_list), 'dictionary': word_dictionary.items()})
